7aa877bf62a2d13a68801db4a4210553852f9912
Tim Wei <galadiya41@gmail.com>
2021-07-13 14:13:10 +0800

    feat: add background script to listen history change

---
+++ src/background.js
@@ -0,0 +1,26 @@
+import { GLOBAL_MESSAGE_TYPE } from 'constants'
+
+const activeTabUrlMemo = {}
+
+/**
+ * To support Github SPA
+ * ref: https://developer.chrome.com/docs/extensions/reference/webNavigation/
+ */
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+  ({ tabId, url }) => {
+    if (activeTabUrlMemo[tabId] !== url) {
+      activeTabUrlMemo[tabId] = url
+
+      chrome.tabs.sendMessage(tabId, {
+        type: GLOBAL_MESSAGE_TYPE.ON_HISTORY_UPDATED,
+      })
+    }
+  },
+  { url: [{ hostSuffix: 'github.com' }] }
+)
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  if (activeTabUrlMemo[tabId]) {
+    delete activeTabUrlMemo[tabId]
+  }
+})
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,5 @@
 export const CONTAINER_ID = 'github-review-enhancer'
+
+export const GLOBAL_MESSAGE_TYPE = {
+  ON_HISTORY_UPDATED: 'ON_HISTORY_UPDATED',
+}
