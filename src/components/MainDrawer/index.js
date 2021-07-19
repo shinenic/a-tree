a442c3f9f4e079a81870e20ede75eecb870600be
KKzLEO <kkleo@ntut.org.tw>
2021-07-19 20:19:10 +0800

    feat(drawer): implement <MainDrawer>

---
+++ src/components/MainDrawer/index.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Drawer from '@material-ui/core/Drawer'
+import usePageInfo from 'hooks/usePageInfo'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { makeStyles } from '@material-ui/core/styles'
+import { PAGE_TYPE } from 'constants'
+
+import { useQueryRepoInfo } from 'hooks/api/useGithubQueries'
+import CodePage from 'components/pages/Code'
+import PRPage from 'components/pages/PR'
+
+import { isEmpty, compact } from 'lodash'
+import * as Style from './style'
+
+const useStyles = makeStyles({
+  paper: {
+    width: 400,
+    // borderRightColor: 'black',
+  },
+})
+
+const MainDrawer = () => {
+  const classes = useStyles()
+
+  const {
+    pageType,
+    owner,
+    repo,
+    commit,
+    pull,
+    branch: branchFromUrl,
+    filePath,
+  } = usePageInfo()
+  const { data, loading, error } = useQueryRepoInfo({ owner, repo })
+
+  if (loading || isEmpty(data)) return null
+
+  const { default_branch: defaultBranch } = data
+  const branch = branchFromUrl || defaultBranch
+
+  const renderHeader = () => {
+    let breadcrumb = [owner, repo]
+
+    if (pageType === PAGE_TYPE.CODE) {
+      breadcrumb.push(branch)
+      breadcrumb.push(filePath)
+    }
+
+    if (pageType === PAGE_TYPE.COMMIT) {
+      breadcrumb.push(branch)
+      breadcrumb.push(commit)
+    }
+
+    if (pageType === PAGE_TYPE.PR) {
+      breadcrumb.push(pull)
+    }
+
+    if (pageType === PAGE_TYPE.PR_COMMIT) {
+      breadcrumb.push(pull)
+      breadcrumb.push(commit)
+    }
+
+    return compact(breadcrumb).join('  >  ')
+  }
+
+  const renderContent = () => {
+    switch (pageType) {
+      case PAGE_TYPE.CODE:
+      default:
+        return <CodePage owner={owner} repo={repo} branch={branch} />
+      case PAGE_TYPE.PR:
+        return <PRPage owner={owner} repo={repo} pr={commit} />
+    }
+  }
+
+  return (
+    <Drawer anchor="left" open variant="permanent" classes={classes}>
+      <Style.DrawerHeader>{renderHeader()}</Style.DrawerHeader>
+      <Style.DrawerContent>{renderContent()}</Style.DrawerContent>
+    </Drawer>
+  )
+}
+
+MainDrawer.propTypes = {}
+
+export default MainDrawer
