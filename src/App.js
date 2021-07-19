b56ae11a138ed87620308fac316a122721d49e28
KKzLEO <kkleo@ntut.org.tw>
2021-07-19 20:20:00 +0800

    feat(app): add <MainDrawer> and <GlobalStyle>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import GlobalStyle from './GlobalStyle'
+import MainDrawer from 'components/MainDrawer'
 import usePageInfo from 'hooks/usePageInfo'
+import { PAGE_TYPE } from 'constants'
 
 function App() {
   const pageInfo = usePageInfo()
-  console.log({ ...pageInfo })
 
-  return <div />
+  console.log('pageInfo', pageInfo)
+  if (pageInfo.pageType === PAGE_TYPE.UNKNOWN) return null
+
+  return (
+    <>
+      <GlobalStyle />
+      <MainDrawer {...pageInfo} />
+    </>
+  )
 }
 
 export default App
