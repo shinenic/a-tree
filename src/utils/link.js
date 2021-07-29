6732f2b7e7e2879c8fdd0aa26652d7f851567808
Tim Wei <galadiya41@gmail.com>
2021-07-29 23:03:10 +0800

    fix: correct missed pjax id

--- src/utils/link.js
+++ src/utils/link.js
@@ -1,10 +1,10 @@
-import { PJAX_LINK_ID } from 'constants/github'
+import { PJAX_ID } from 'constants/github'
 
 export const getFileLink = ({ owner, repo, type, branch, filePath }) => {
   return `${window.location.origin}/${owner}/${repo}/${type}/${branch}/${filePath}`
 }
 
-export const linkGithubPage = (url, pjaxId = PJAX_LINK_ID) => {
+export const linkGithubPage = (url, pjaxId = PJAX_ID.CODE) => {
   const link = document.createElement('a')
   link.setAttribute('href', url)
   link.setAttribute('data-pjax', pjaxId)
