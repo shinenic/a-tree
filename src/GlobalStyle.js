c3a94a9d92e9b5f9a2ff383ad0d7e785082944ef
KKzLEO <kkleo@ntut.org.tw>
2021-09-09 11:08:07 +0800

    feat(app): allow displaying extension by domain

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
-    margin-left: ${(props) => props.pl}px;
+    margin-left: ${(props) => props.pl}px !important;
   }
 `
 
--- src/index.js
+++ src/index.js
@@ -5,8 +5,25 @@ import { CONTAINER_ID } from 'constants'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import SettingProvider from 'components/Setting/Context/Provider'
 import GenerateTokenGuide from 'components/Tour/GenerateTokenGuide'
+import { getSettingFromLocalStorage } from 'utils/setting'
 
-const queryClient = new QueryClient()
+const checkDomainMatched = () => {
+  const host = window.location.host
+
+  return host.includes('github')
+}
+
+const applyStyleFromLocalStorage = () => {
+  const { drawerWidth } = getSettingFromLocalStorage()
+
+  const style = document.createElement('style')
+  style.innerHTML = `
+    body {
+      margin-left: ${drawerWidth}px !important;
+    }
+  `
+  document.documentElement.appendChild(style)
+}
 
 const createContainer = () => {
   const container = document.createElement('div')
@@ -15,22 +32,28 @@ const createContainer = () => {
 }
 
 const renderExtension = () => {
-  createContainer()
+  if (!checkDomainMatched()) return
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <SettingProvider>
-          <GenerateTokenGuide />
-          <App />
-        </SettingProvider>
-      </QueryClientProvider>
-    </React.StrictMode>,
-    document.getElementById(CONTAINER_ID)
-  )
+  applyStyleFromLocalStorage()
+
+  const onLoad = () => {
+    const queryClient = new QueryClient()
+
+    createContainer()
+
+    ReactDOM.render(
+      <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <SettingProvider>
+            <GenerateTokenGuide />
+            <App />
+          </SettingProvider>
+        </QueryClientProvider>
+      </React.StrictMode>,
+      document.getElementById(CONTAINER_ID)
+    )
+  }
+  window.onload = onLoad
 }
 
-/**
- * @TODO Handle margin of `body` for width of drawer
- */
-window.onload = renderExtension
+renderExtension()
