ec974ba57e06d316087cfef5e80010426904f5fb
KKz <kkleo@ntut.org.tw>
2021-09-18 23:35:06 +0800

    feat(floating button): implement floating button to control drawer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import MainDrawer from 'components/MainDrawer'
 import usePageInfo from 'hooks/pageInfo/usePageInfo'
 import FileSearchModal from 'components/FileSearchModal'
 import { useSettingStateCtx } from 'components/Setting/Context/Provider'
+import FloatingButton from 'components/FloatingButton'
 import GlobalStyle from './GlobalStyle'
 
 function App() {
   const { error, isLoading, pageInfo } = usePageInfo()
-  const { drawerWidth } = useSettingStateCtx()
+  const { drawerWidth, drawerPinned } = useSettingStateCtx()
 
   return (
     <>
-      <GlobalStyle pl={drawerWidth} />
-      <MainDrawer {...pageInfo} error={error} isLoading={isLoading} />
+      <FloatingButton />
+      <GlobalStyle pl={drawerPinned ? drawerWidth : 0} />
+      <MainDrawer
+        {...pageInfo}
+        error={error}
+        isLoading={isLoading}
+        open={drawerPinned}
+      />
       <FileSearchModal {...pageInfo} />
     </>
   )
---
+++ src/components/FloatingButton/index.js
@@ -0,0 +1,79 @@
+import React, { useMemo } from 'react'
+import Box from '@material-ui/core/Box'
+import Draggable from 'react-draggable'
+import { useSettingCtx } from 'components/Setting/Context/Provider'
+
+const HEIGHT = 50
+
+const FloatingButton = () => {
+  const [{ floatingButtonPositionY }, dispatch] = useSettingCtx()
+
+  // Memorized original position y because the implementation of `react-draggable` is
+  // to use `transform` to control the element, if we update position y immediately,
+  // the position y would be doubled (position Y + transform Y),
+  // so we need to keep original Y to make <Draggable> controllable and
+  // update local storage's Y as soon as we finished dragging.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const originalPositionY = useMemo(() => floatingButtonPositionY, [])
+
+  const url = window.chrome.runtime.getURL('icon192.png')
+
+  return (
+    <Box position="fixed" top={originalPositionY} left={0} zIndex={2100}>
+      <Draggable
+        axis="y"
+        defaultPosition={{ x: 0, y: 0 }}
+        handle="span"
+        onStop={(_, { y }) => {
+          dispatch({
+            type: 'UPDATE_FLOATING_BUTTON_POSITION_Y',
+            payload: y + originalPositionY,
+          })
+        }}
+      >
+        <Box
+          width={50}
+          height={HEIGHT}
+          bgcolor="#252a2e"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          borderRadius="0 5px 5px 0"
+          style={{
+            cursor: 'pointer',
+          }}
+          onClick={() => {
+            dispatch({ type: 'TOGGLE_DRAWER' })
+          }}
+        >
+          <Box
+            component="img"
+            src={url}
+            width={30}
+            height={30}
+            style={{
+              userSelect: 'none',
+              userDrag: 'none',
+            }}
+          />
+          <Box
+            position="absolute"
+            width={20}
+            height={HEIGHT}
+            top={0}
+            left={0}
+            component="span"
+            style={{
+              cursor: 'move',
+            }}
+            onClick={(e) => e.stopPropagation()}
+          />
+        </Box>
+      </Draggable>
+    </Box>
+  )
+}
+
+FloatingButton.propTypes = {}
+
+export default FloatingButton
