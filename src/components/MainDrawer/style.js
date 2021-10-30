c268ab94ac27f1f4592e5970a711b481d62c92f1
KKz <kkleo@ntut.org.tw>
2021-10-31 03:15:45 +0800

    feat(Header): adjust the color of header in dark mode

--- src/components/MainDrawer/style.js
+++ src/components/MainDrawer/style.js
@@ -3,12 +3,17 @@ import { getCommonScrollbarStyle } from 'utils/style'
 import { styled as muiStyled } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Box from '@material-ui/core/Box'
+import { useTheme } from '@material-ui/core'
 
 export const DrawerHeader = ({ selected, height, ...props }) => {
+  const theme = useTheme()
+
+  const bgColor = theme.palette.type === 'dark' ? '#141a1e' : 'rgb(55, 62, 67)'
+
   return (
     <Box
       sx={{
-        background: 'rgb(55, 62, 67)',
+        background: bgColor,
         minHeight: height || DEFAULT_HEADER_HEIGHT,
         boxSizing: 'border-box',
         color: '#f9f9f9',
