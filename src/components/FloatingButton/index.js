39a86d97d0ba76b307e5d2ed53bc16eb7fc1c9ec
Tim Wei <galadiya41@gmail.com>
2021-11-15 20:04:15 +0800

    refactor: custom draggable floating button

--- src/components/FloatingButton/index.js
+++ src/components/FloatingButton/index.js
@@ -1,17 +1,11 @@
-import React, { useMemo, useCallback, useEffect, useRef } from 'react'
 import Box from '@material-ui/core/Box'
-import Draggable from 'react-draggable'
 import useSettingStore from 'stores/setting'
 import { makeStyles } from '@material-ui/core/styles'
 import { getURL } from 'utils/chrome'
-import useWindowSize from 'hooks/useWindowSize'
-import { getHeaderHeight } from 'utils/style'
-import { DEFAULT_HEADER_HEIGHT } from 'constants'
+import useGetFloatingButtonProps from 'hooks/useGetFloatingButtonProps'
 
 const BOX_SIZE = 40
 const IMG_WIDTH = 30
-
-const TOGGLE_THRESHOLD = 6
 
 const iconUrl = getURL('icon192.png')
 
@@ -42,90 +36,40 @@ const useStyles = ({ drawerPinned }) =>
  *       Or close drawer automatically on specific page types
  */
 const FloatingButton = () => {
-  const floatingButtonPositionY = useSettingStore((s) => s.floatingButtonPositionY)
   const drawerPinned = useSettingStore((s) => s.drawerPinned)
-  const dispatch = useSettingStore((s) => s.dispatch)
 
-  const { height: windowHeight } = useWindowSize()
-  const originY = useRef(floatingButtonPositionY)
   const containerClasses = useStyles({ drawerPinned })()
-
-  const headerHeight = useMemo(() => {
-    const heightNumber = Number(getHeaderHeight().replace(/\D+/g, ''))
-    return Number.isNaN(heightNumber) || heightNumber === 0
-      ? DEFAULT_HEADER_HEIGHT
-      : heightNumber
-  }, [])
-
-  const onStart = useCallback((_, { y }) => {
-    originY.current = y
-  }, [])
-
-  const onStop = useCallback((_, { y }) => {
-    if (Math.abs(y - originY.current) < TOGGLE_THRESHOLD) {
-      dispatch({ type: 'TOGGLE_DRAWER' })
-    }
-  }, [])
-
-  const updateY = useCallback((_, { y }) => {
-    dispatch({
-      type: 'UPDATE_FLOATING_BUTTON_POSITION_Y',
-      payload: y,
-    })
-  }, [])
-
-  useEffect(() => {
-    if (windowHeight - BOX_SIZE < floatingButtonPositionY) {
-      dispatch({
-        type: 'UPDATE_FLOATING_BUTTON_POSITION_Y',
-        payload: windowHeight - BOX_SIZE,
-      })
-    }
-  }, [windowHeight])
+  const getFloatingButtonProps = useGetFloatingButtonProps({
+    boxSize: BOX_SIZE,
+  })
 
   return (
-    <Box position="fixed" top={0} left={0} zIndex={2100}>
-      <Draggable
-        axis="y"
-        defaultPosition={{ x: floatingButtonPositionY, y: 0 }}
-        handle="span"
-        onDrag={updateY}
-        position={{ y: floatingButtonPositionY, x: 0 }}
-        bounds={{
-          top: headerHeight,
-          bottom: windowHeight - BOX_SIZE,
-        }}
-        onStart={onStart}
-        onStop={onStop}
-      >
-        <Box classes={containerClasses}>
-          <Box
-            component="img"
-            src={iconUrl}
-            width={IMG_WIDTH}
-            height={IMG_WIDTH}
-            style={{
-              userSelect: 'none',
-              userDrag: 'none',
-            }}
-          />
-          <Box
-            position="absolute"
-            width={BOX_SIZE}
-            height={BOX_SIZE}
-            top={0}
-            left={0}
-            component="span"
-            style={{
-              cursor: 'move',
-            }}
-          />
-        </Box>
-      </Draggable>
+    <Box {...getFloatingButtonProps()}>
+      <Box classes={containerClasses}>
+        <Box
+          component="img"
+          src={iconUrl}
+          width={IMG_WIDTH}
+          height={IMG_WIDTH}
+          style={{
+            userSelect: 'none',
+            userDrag: 'none',
+          }}
+        />
+        <Box
+          position="absolute"
+          width={BOX_SIZE}
+          height={BOX_SIZE}
+          top={0}
+          left={0}
+          component="span"
+          style={{
+            cursor: 'move',
+          }}
+        />
+      </Box>
     </Box>
   )
 }
-
-FloatingButton.propTypes = {}
 
 export default FloatingButton
---
+++ src/hooks/useGetFloatingButtonProps.js
@@ -0,0 +1,114 @@
+import { useMemo, useCallback, useEffect, useRef, useState } from 'react'
+import useSettingStore from 'stores/setting'
+import useWindowSize from 'hooks/useWindowSize'
+import { getHeaderHeight } from 'utils/style'
+import { DEFAULT_HEADER_HEIGHT } from 'constants'
+
+const BOX_SIZE = 40
+const TOGGLE_THRESHOLD = 3
+
+// support only Y-axis
+const useGetFloatingButtonProps = ({
+  boxSize = 0,
+  dragThreshold = TOGGLE_THRESHOLD,
+} = {}) => {
+  const positionY = useSettingStore((s) => s.floatingButtonPositionY)
+  const dispatch = useSettingStore((s) => s.dispatch)
+  const { height: windowHeight } = useWindowSize()
+
+  const [isMouseDown, setIsMouseDown] = useState(false)
+
+  const startPageYRef = useRef(null)
+  const startYRef = useRef(null)
+
+  const updatePositionY = useCallback(
+    (y) =>
+      dispatch({
+        type: 'UPDATE_FLOATING_BUTTON_POSITION_Y',
+        payload: y,
+      }),
+    [dispatch]
+  )
+
+  const topBound = useMemo(() => {
+    const heightNumber = Number(getHeaderHeight().replace(/\D+/g, ''))
+    return Number.isNaN(heightNumber) || heightNumber === 0
+      ? DEFAULT_HEADER_HEIGHT
+      : heightNumber
+  }, [])
+
+  const onMouseDown = (e) => {
+    setIsMouseDown(true)
+
+    startPageYRef.current = e.pageY
+    startYRef.current = positionY
+  }
+
+  useEffect(() => {
+    if (!isMouseDown) return
+
+    let isOverThreshold = false
+
+    const onMouseUp = () => {
+      setIsMouseDown(false)
+
+      if (!isOverThreshold) {
+        dispatch({ type: 'TOGGLE_DRAWER' })
+      }
+    }
+
+    const onMouseMove = (e) => {
+      e.preventDefault() // prevent select background unexpectedly
+      const deltaY = e.pageY - startPageYRef.current
+
+      if (Math.abs(deltaY) < dragThreshold) return
+
+      isOverThreshold = true
+
+      const newPositionY = startYRef.current + deltaY
+      const bottomBound = windowHeight - boxSize
+
+      if (newPositionY < topBound) {
+        updatePositionY(topBound)
+      } else if (newPositionY > bottomBound) {
+        updatePositionY(bottomBound)
+      } else {
+        updatePositionY(newPositionY)
+      }
+    }
+
+    // Listen whole document after mouse down to increase the detection area
+    document.addEventListener('mouseup', onMouseUp, true)
+    document.addEventListener('mousemove', onMouseMove, true)
+
+    return () => {
+      document.removeEventListener('mouseup', onMouseUp, true)
+      document.removeEventListener('mousemove', onMouseMove, true)
+    }
+  }, [isMouseDown]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    if (windowHeight - BOX_SIZE < positionY) {
+      dispatch({
+        type: 'UPDATE_FLOATING_BUTTON_POSITION_Y',
+        payload: windowHeight - BOX_SIZE,
+      })
+    }
+  }, [windowHeight]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  console.log({ positionY, topBound })
+
+  return () => ({
+    onMouseDown,
+    style: {
+      transform: `translateY(${positionY}px)`,
+      transition: 'transform .1s',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      zIndex: 2100,
+    },
+  })
+}
+
+export default useGetFloatingButtonProps
