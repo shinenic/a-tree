170e485e7971ac4614c1319e9095277cb3e42ff7
Tim Wei <galadiya41@gmail.com>
2021-12-20 22:53:02 +0800

    fix: update deps of tree data

--- src/hooks/tree/useQueryTree.js
+++ src/hooks/tree/useQueryTree.js
@@ -2,14 +2,14 @@ import {
   useQueryFiles,
   useQueryPull,
   useQueryCommit,
-  useQueryMultiCommits,
+  useQueryMultiCommits
 } from 'hooks/api/useGithubQueries'
 import { useMemo } from 'react'
 import {
   PAGE_TYPE,
   PAGES_WITH_BRANCH_TREE,
   PULL_PAGE_TYPE,
-  PAGES_WITH_FULL_PULL_TREE,
+  PAGES_WITH_FULL_PULL_TREE
 } from 'constants/base'
 
 const useQueryTree = (pageInfo, enabled = true) => {
@@ -17,15 +17,15 @@ const useQueryTree = (pageInfo, enabled = true) => {
 
   const queryPull = useQueryPull(
     { owner, pull, repo },
-    { enabled: enabled && PAGES_WITH_FULL_PULL_TREE.includes(pageType) }
+    {
+      enabled: enabled && PAGES_WITH_FULL_PULL_TREE.includes(pageType)
+    }
   )
 
   const queryCommit = useQueryCommit(
     { owner, commit, repo },
     {
-      enabled:
-        enabled &&
-        [PAGE_TYPE.PULL_COMMIT, PAGE_TYPE.CODE_COMMIT].includes(pageType),
+      enabled: enabled && [PAGE_TYPE.PULL_COMMIT, PAGE_TYPE.CODE_COMMIT].includes(pageType)
     }
   )
 
@@ -37,7 +37,7 @@ const useQueryTree = (pageInfo, enabled = true) => {
   const queryMultiCommits = useQueryMultiCommits(
     { owner, repo, baseCommit: commit?.[0], headCommit: commit?.[1] },
     {
-      enabled: enabled && pageType === PAGE_TYPE.PULL_COMMITS,
+      enabled: enabled && pageType === PAGE_TYPE.PULL_COMMITS
     }
   )
 
@@ -48,12 +48,11 @@ const useQueryTree = (pageInfo, enabled = true) => {
     [PAGE_TYPE.PULL_COMMIT]: queryCommit,
     [PAGE_TYPE.CODE_COMMIT]: queryCommit,
 
-    [PAGE_TYPE.PULL_COMMITS]: queryMultiCommits,
+    [PAGE_TYPE.PULL_COMMITS]: queryMultiCommits
   }
 
   const { data, isLoading, error } = queryMap[pageType] ?? queryFiles
 
-  const hasData = Boolean(data)
   const files = useMemo(
     /**
      * @note Files in each different API
@@ -62,14 +61,14 @@ const useQueryTree = (pageInfo, enabled = true) => {
      *  pull:   data
      */
     () => data?.files || data?.tree || data,
-    [pageType, owner, repo, commit, pull, branch, hasData] // eslint-disable-line
+    [data]
   )
 
   return {
     files,
     isLoading,
     error,
-    isLargeRepoTree: data?.truncated && !PULL_PAGE_TYPE[pageType],
+    isLargeRepoTree: data?.truncated && !PULL_PAGE_TYPE[pageType]
   }
 }
 
