eae824f7650fe6c2d1afc17db6b6d7f27f9d68ee
Tim Wei <galadiya41@gmail.com>
2021-12-21 21:55:46 +0800

    style: bracket line break

--- src/hooks/tree/useQueryTree.js
+++ src/hooks/tree/useQueryTree.js
@@ -17,16 +17,12 @@ const useQueryTree = (pageInfo, enabled = true) => {
 
   const queryPull = useQueryPull(
     { owner, pull, repo },
-    {
-      enabled: enabled && PAGES_WITH_FULL_PULL_TREE.includes(pageType)
-    }
+    { enabled: enabled && PAGES_WITH_FULL_PULL_TREE.includes(pageType) }
   )
 
   const queryCommit = useQueryCommit(
     { owner, commit, repo },
-    {
-      enabled: enabled && [PAGE_TYPE.PULL_COMMIT, PAGE_TYPE.CODE_COMMIT].includes(pageType)
-    }
+    { enabled: enabled && [PAGE_TYPE.PULL_COMMIT, PAGE_TYPE.CODE_COMMIT].includes(pageType) }
   )
 
   const queryFiles = useQueryFiles(
@@ -36,9 +32,7 @@ const useQueryTree = (pageInfo, enabled = true) => {
 
   const queryMultiCommits = useQueryMultiCommits(
     { owner, repo, baseCommit: commit?.[0], headCommit: commit?.[1] },
-    {
-      enabled: enabled && pageType === PAGE_TYPE.PULL_COMMITS
-    }
+    { enabled: enabled && pageType === PAGE_TYPE.PULL_COMMITS }
   )
 
   const queryMap = {
